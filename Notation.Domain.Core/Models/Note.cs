3dd6714f159e2cbf9e98ff31b4bb5e8bb08399fb
Bohomaz Oleksii <o.bohomaz@andersenlab.com>
2022-07-11 22:53:43 +0300

    create notes entity

---
+++ Notation.Domain.Core/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notation.Domain.Core.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public int CategoryId { get; set; }
+        public int AuthorId { get; set; }
+        public int TypeNoteId { get; set; }
+        public int StatusId { get; set; }
+        public DateTime TimeCreate { get; set; }
+    }
+}
