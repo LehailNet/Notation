c796acefd7cde7f80f00f91a77ea6d4b66a9f1fa
Bohomaz Oleksii <o.bohomaz@andersenlab.com>
2022-07-12 18:32:41 +0300

    commit for remote

--- Notation.Domain.Intarfaces/ICategoryRepository.cs
+++ Notation.Domain.Intarfaces/ICategoryRepository.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Notation.Domain.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Notation.Domain.Intarfaces
 {
-    internal interface Interface1
+    internal interface ICategoryRepository
     {
+        Task CreateCategory(Category category);
+
+        Category GetCategory(int id);
+
+        void UpdateCategory(Category category);
+
+        void DeleteCategory(int id);
+
+        IEnumerable<Category> GetCategories();
     }
 }
--- Notation.Domain.Intarfaces/INoteRepository.cs
+++ Notation.Domain.Intarfaces/INoteRepository.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Notation.Domain.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Notation.Domain.Intarfaces
 {
     internal interface INoteRepository
     {
+        Task CreateNote(Note note);
+
+        Note GetNote(int id);
+
+        void UpdateNote(Note note);
+
+        void DeleteNote(int id);
+
+        IEnumerable<Note> GetNotes();
     }
 }
