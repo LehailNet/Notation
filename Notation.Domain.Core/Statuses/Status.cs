7f433bec8f5842a524fe0912d6f6aa87693e9840
Bohomaz Oleksii <o.bohomaz@andersenlab.com>
2022-07-12 11:29:57 +0300

    create status entity

---
+++ Notation.Domain.Core/Statuses/Status.cs
@@ -0,0 +1,14 @@
+﻿using Notation.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notation.Domain.Core.Statuses
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Note> Notes { get; set; }
+    }
+}
