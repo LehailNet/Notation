45262eed4a8ea68ed43499d42d007041ad44a0c5
Bohomaz Oleksii <o.bohomaz@andersenlab.com>
2022-07-12 11:23:02 +0300

    create category entity

---
+++ Notation.Domain.Core/Categories/Category.cs
@@ -0,0 +1,14 @@
+﻿using Notation.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notation.Domain.Core.Categories
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Note> Notes { get; set; }
+    }
+}
