0cee8d0d7ba1293adacfe79453195388af485587
Bohomaz Oleksii <o.bohomaz@andersenlab.com>
2022-07-12 11:25:02 +0300

    create author entity

---
+++ Notation.Domain.Core/Authors/Author.cs
@@ -0,0 +1,14 @@
+﻿using Notation.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notation.Domain.Core.Authors
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Note> Notes { get; set; }
+    }
+}
