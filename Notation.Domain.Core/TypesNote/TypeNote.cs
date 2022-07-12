0ff0f8aefbdce495fb6f6ee97223cadc0ef014dd
Bohomaz Oleksii <o.bohomaz@andersenlab.com>
2022-07-12 11:27:02 +0300

    create types note entity

---
+++ Notation.Domain.Core/TypesNote/TypeNote.cs
@@ -0,0 +1,14 @@
+﻿using Notation.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notation.Domain.Core.TypesNote
+{
+    public class TypeNote
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Note> Notes { get; set; }
+    }
+}
