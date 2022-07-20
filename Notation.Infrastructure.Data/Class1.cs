b191a25cb84a0585f1a7a391087d30630d7c1a3a
Bohomaz Oleksii <o.bohomaz@andersenlab.com>
2022-07-20 22:26:36 +0300

    create dbContext

--- Notation.Infrastructure.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Notation.Infrastructure.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ Notation.Infrastructure.Data/DbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+
+namespace Notation.Infrastructure.Data
+{
+    public class DbContext
+    {
+        private MongoClient _client;
+        private IConfiguration _configuration;
+
+        public DbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public MongoClient GetClient ()
+        {
+            if (_client != null)
+                return _client;
+
+            _client = new MongoClient (_configuration.GetConnectionString("MongoDb"));
+            return _client;
+
+        }
+    }
+}
