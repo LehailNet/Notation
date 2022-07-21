9c65b86ff74e27f251364af5c3482d2d35d340bb
Bohomaz Oleksii <o.bohomaz@andersenlab.com>
2022-07-21 20:09:58 +0300

    create mongoDbContext

---
+++ Notation.Infrastructure.Data/MongoDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Notation.Infrastructure.Data
+{
+    public class MongoDbContext
+    {
+        private string _connectionString;
+
+        private MongoClient client;
+        private IMongoDatabase database;
+
+        public MongoDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IMongoDatabase GetMongoDB ()
+        {
+            if (database != null)
+                return database;
+
+            client = new MongoClient (_connectionString);
+            database = client.GetDatabase("notation");
+
+            return database;
+        }
+    }
+}
