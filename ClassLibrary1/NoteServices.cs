73bd1dedc89a97b0c15852be529d4c5720048971
Bohomaz Oleksii <o.bohomaz@andersenlab.com>
2022-08-29 23:47:47 +0300

    create method for add notation

---
+++ ClassLibrary1/NoteServices.cs
@@ -0,0 +1,25 @@
+﻿using Notation.Domain.Core.Models;
+using Notation.Domain.Intarfaces;
+using Notation.Services.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class NoteServices : INoteServices
+    {
+
+        private readonly IBaseRepository<Note> _repository;
+
+        public NoteServices(IBaseRepository<Note> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task CreateNoteAsync(Note note)
+        {
+            Thread.Sleep(4000);
+            await _repository.CreateAsync(note);
+        }
+    }
+}
---
+++ Notation.Services.Interfaces/INoteServices.cs
@@ -0,0 +1,10 @@
+﻿using Notation.Domain.Core.Models;
+using System.Threading.Tasks;
+
+namespace Notation.Services.Interfaces
+{
+    public interface INoteServices
+    {
+        Task CreateNoteAsync(Note note);
+    }
+}
